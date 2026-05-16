dd3f449a27b6be0125d0d9847b750579fe751079
Shyam James <shyamjames74@gmail.com>
2026-05-17 00:56:51 +0530

    add moon's gravitational interactions and update output for moon's position

--- orbital.c
+++ orbital.c
@@ -28,19 +28,41 @@ void update(struct Body *b, double fx, double fy, double dt){
 int main(){
     struct Body sun = {"Sun",1.989e30,0,0,0,0};
     struct Body earth = {"Earth", 5.972e24, 0, 1.496e11, 29780, 0};
-    
+    struct Body moon = {"Moon", 7.342e22, 0, 1.496e11+3.944e8, 29780+1022, 0};
+
     double dt = 3600; // one hour
 
     for(int i=0;i<8760;i++){
-        double dx = sun.x - earth.x;
+        //force on earth from sun
+        double dx = sun.x - earth.x; 
         double dy = sun.y  - earth.y;
-        double dist =  sqrt(dx*dx + dy*dy);
+        double dist =  sqrt(dx*dx + dy*dy); // actual distance
         double force = gravitational_force(earth,sun);
-        double fx = force*(dx/dist);
-        double fy = force*(dy/dist);
+        double fx = force*(dx/dist); // force in x dir
+        double fy = force*(dy/dist); // force in y dir
         update(&earth,fx,fy,dt);
+
+        //force on moon from sun
+        double mdx = sun.x - moon.x;
+        double mdy = sun.y - moon.y;
+        double mdist_s = sqrt(mdx*mdx + mdy*mdy);
+        double mforce_s = gravitational_force(moon,sun);
+        double mfx_s = force*(mdx/mdist_s);
+        double mfy_s = force*(mdy/mdist_s);
+
+        //force on moon from earth
+        double mdx_e = earth.x - moon.x;
+        double mdy_e = earth.y - moon.y;
+        double mdist_e = sqrt(mdx_e*mdx_e + mdy_e*mdy_e);
+        double mforce_e = gravitational_force(moon,earth);
+        double mfx_e = force*(mdx_e/mdist_e);
+        double mfy_e = force*(mdy_e/mdist_e);
+
+        update(&moon,mfx_e+mfx_s,mfy_e+mfy_s,dt);
+        
         if(i%720==0){
-            printf("Hour %2d: position: (%.3e, %.3e)\n",i+1,earth.x,earth.y);
+            printf("Earth: Hour %2d: position: (%.3e, %.3e)\n",i+1,earth.x,earth.y);
+            printf("Moon: Hour %2d: position: (%.3e, %.3e)\n",i+1,moon.x,moon.y);
         }
     }
     return 0;
